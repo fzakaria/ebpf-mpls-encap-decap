a06c7480410e61356e1bc8f727f62d21b59550f5
Farid Zakaria <farid.m.zakaria@gmail.com>
2019-01-10 20:44:36 -0800

    Added mpls.h

---
+++ mpls.h
@@ -0,0 +1,120 @@
+#ifndef MPLS_H
+#define MPLS_H
+
+#define MPLS_HLEN 4
+
+/**
+ * Have our own version of mpls.h since OracleLinux  does not include newer functionality
+ * like encode & decode.
+ * sources: https://elixir.bootlin.com/linux/latest/source/include/uapi/linux/mpls.h &
+ * https://github.com/torvalds/linux/blob/6f0d349d922ba44e4348a17a78ea51b7135965b1/net/mpls/internal.h
+ */
+
+#include "bpf_endian.h"
+#include <stdbool.h> 
+
+#define MPLS_LS_LABEL_MASK      0xFFFFF000
+#define MPLS_LS_LABEL_SHIFT     12
+#define MPLS_LS_TC_MASK         0x00000E00
+#define MPLS_LS_TC_SHIFT        9
+#define MPLS_LS_S_MASK          0x00000100
+#define MPLS_LS_S_SHIFT         8
+#define MPLS_LS_TTL_MASK        0x000000FF
+#define MPLS_LS_TTL_SHIFT       0
+
+/* Reserved labels */
+#define MPLS_LABEL_IPV4NULL		0 /* RFC3032 */
+#define MPLS_LABEL_RTALERT		1 /* RFC3032 */
+#define MPLS_LABEL_IPV6NULL		2 /* RFC3032 */
+#define MPLS_LABEL_IMPLNULL		3 /* RFC3032 */
+#define MPLS_LABEL_ENTROPY		7 /* RFC6790 */
+#define MPLS_LABEL_GAL			13 /* RFC5586 */
+#define MPLS_LABEL_OAMALERT		14 /* RFC3429 */
+#define MPLS_LABEL_EXTENSION		15 /* RFC7274 */
+
+#define MPLS_LABEL_FIRST_UNRESERVED	16 /* RFC3032 */
+
+/* Reference: RFC 5462, RFC 3032
+ *
+ *  0                   1                   2                   3
+ *  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                Label                  | TC  |S|       TTL     |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *
+ *	Label:  Label Value, 20 bits
+ *	TC:     Traffic Class field, 3 bits
+ *	S:      Bottom of Stack, 1 bit
+ *	TTL:    Time to Live, 8 bits
+ */
+struct mpls_shim_hdr {
+	unsigned int label_stack_entry;
+};
+
+struct mpls_entry_decoded {
+	unsigned int label;
+	unsigned char ttl;
+	unsigned char tc;
+	unsigned char bos;
+	//inserted to make -Wpadded happy
+	unsigned char align_padding;
+};
+
+/*
+ * check if the protocol type is either MPLS unicast or MPLS multicast
+ */
+static inline bool is_eth_p_mpls(unsigned short eth_type)
+{
+	return eth_type == bpf_htons(ETH_P_MPLS_UC) || eth_type == bpf_htons(ETH_P_MPLS_MC);
+}
+
+/*
+ * test whether mpl shim header is the bottom of the stack.
+ * @param hdr the mpls header to test
+ */
+static inline bool is_mpls_entry_bos(struct mpls_shim_hdr * hdr)
+{
+	return hdr->label_stack_entry & bpf_htonl(MPLS_LS_S_MASK);
+}
+
+
+/*
+ * encode the values into the network header.
+ * @label the 20bits of MPLS label
+ * @ttl time to live
+ * @tc traffic class
+ * @bos true/false if label is the bottom of the stack
+ */
+static inline struct mpls_shim_hdr mpls_encode(unsigned int label, unsigned int ttl,
+													 unsigned int tc, int bos)
+{
+	struct mpls_shim_hdr result;
+	result.label_stack_entry =
+		//we need to convert from CPU endian to network endian
+		bpf_htonl((label << MPLS_LS_LABEL_SHIFT) |
+			    (tc << MPLS_LS_TC_SHIFT) |
+			    (bos ? (1 << MPLS_LS_S_SHIFT) : 0) |
+			    (ttl << MPLS_LS_TTL_SHIFT));
+	return result;
+}
+
+/*
+ * decode the header into a friendlier struct for easy access.
+ * @hdr the shim header
+ */
+static inline struct mpls_entry_decoded mpls_entry_decode(struct mpls_shim_hdr *hdr)
+{
+	struct mpls_entry_decoded result;
+	//we need to convert from network endian to host endian
+	unsigned int entry = bpf_ntohl(hdr->label_stack_entry);
+
+	result.label = (entry & MPLS_LS_LABEL_MASK) >> MPLS_LS_LABEL_SHIFT;
+	result.ttl = (entry & MPLS_LS_TTL_MASK) >> MPLS_LS_TTL_SHIFT;
+	result.tc =  (entry & MPLS_LS_TC_MASK) >> MPLS_LS_TC_SHIFT;
+	result.bos = (entry & MPLS_LS_S_MASK) >> MPLS_LS_S_SHIFT;
+
+	return result;
+}
+
+
+#endif //MPLS_H
--- mpls_bpf_kern.c
+++ mpls_bpf_kern.c
@@ -25,13 +25,24 @@
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
-#include <linux/mpls.h>
 #include "bpf_endian.h"
 #include "bpf_helpers.h"
 #include "helpers.h"
+#include "mpls.h"
 
 #define BPF_DROP 2
 #define BPF_OK 0
+
+/*
+ * The Internet Protocol (IP) is defined in RFC 791.
+ * The RFC specifies the format of the IP header.
+ * In the header there is the IHL (Internet Header Length) field which is 4bits
+ * long
+ * and specifies the header length in 32bit words.
+ * The IHL field can hold values from 0 (Binary 0000) to 15 (Binary 1111).
+ * 15 * 32bits = 480bits = 60 bytes
+ */
+#define MAX_IP_HDR_LEN 60
 
 static_assert(sizeof(struct ethhdr) == ETH_HLEN,
               "ethernet header size does not match.");
@@ -76,13 +87,25 @@ SEC("mpls_encap") int mpls_encap(struct __sk_buff *skb) {
    * hton -> host to network order. Network order is always big-endian.
    * pedantic: the protocol is also directly accessible from __sk_buf
    */
-  if (eth->h_proto != bpf_htons(ETH_P_IP)) {
-    bpf_printk("ethernet is not wrapping IP packet.\n");
+  if (!is_eth_p_mpls(eth->h_proto)) {
+    bpf_printk("ethernet is not wrapping MPLS packet.\n");
     return BPF_OK;
   }
 
-  // Why does
-  struct iphdr *iph = (struct iphdr *)(void *)(eth + 1);
+  // mpls shim header is placed between L2 & L3
+  //==[ETH HDR][MPLS HDR][IP HDR][Data]==
+  struct mpls_shim_hdr *mpls = (struct mpls_shim_hdr *)(void *)(eth + 1);
+  if ((void *)(mpls + 1) > data_end) {
+    bpf_printk("socket buffer struct was malformed.\n");
+    return BPF_DROP;
+  }
+
+  if (!is_mpls_entry_bos(mpls)) {
+    bpf_printk("mpls label not bottom of stack.\n");
+    return BPF_DROP;
+  }
+
+  struct iphdr *iph = (struct iphdr *)(void *)(mpls + 1);
 
   if ((void *)(iph + 1) > data_end) {
     bpf_printk("socket buffer struct was malformed.\n");
