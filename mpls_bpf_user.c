b9a1809cb936802b9891358e136c6879857614a2
Farid Zakaria <farid.m.zakaria@gmail.com>
2019-01-13 19:22:55 -0800

    Added user command

--- mpls_bpf_user.c
+++ mpls_bpf_user.c
@@ -1,5 +1,7 @@
+#include <argp.h>
 #include <linux/bpf.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 /*********************************************************************************
@@ -60,9 +62,70 @@ long bpf_map_lookup_elem(unsigned int fd, void *key, void *value) {
 
 /*********************************************************************************/
 
-int main(int argc, char **argv) {
-  int i;
+const char *argp_program_version = "mpls_bpf_user 1.0";
+const char *argp_program_bug_address = "<farid.m.zakaria@gmail.com>";
 
-  printf("argc = %d\n", argc);
-  for (i = 0; i < argc; i++) printf("arg[%d] = \"%s\"\n", i, argv[i]);
+/* Program documentation. */
+static char doc[] = "MPLSoIP User-- a program to interfact with the eBPF code.";
+
+/* A description of the arguments we accept. */
+static char args_doc[] = "[show] [disable|enable]";
+
+/*
+   OPTIONS.  Field 1 in ARGP.
+   Order of fields: {NAME, KEY, ARG, FLAGS, DOC}.
+*/
+static struct argp_option options[] = {
+    {0, 0, 0, 0, 0, 0},
+};
+
+/* This structure is used by main to communicate with parse_opt. */
+struct arguments {
+  void (*cmd)(void);
+};
+
+void show(void);
+void disable(void);
+void enable(void);
+
+void show(void) {}
+
+void disable(void) {}
+
+void enable(void) {}
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+  /* Get the input argument from argp_parse, which we
+   know is a pointer to our arguments structure. */
+  struct arguments *arguments = state->input;
+  switch (key) {
+    case ARGP_KEY_NO_ARGS:
+      argp_usage(state);
+    case ARGP_KEY_ARG:
+      if (strcmp(arg, "show") == 0) {
+        arguments->cmd = &show;
+      } else if (strcmp(arg, "disable") == 0) {
+        arguments->cmd = &disable;
+      } else if (strcmp(arg, "show") == 0) {
+        arguments->cmd = &enable;
+      } else {
+        return ARGP_ERR_UNKNOWN;
+      }
+    default:
+      return ARGP_ERR_UNKNOWN;
+  }
+}
+
+/* Our argp parser. */
+static struct argp argp = {options, parse_opt, args_doc, doc, 0, 0, 0};
+
+int main(int argc, char **argv) {
+  struct arguments arguments;
+  arguments.cmd = NULL;
+  /* Where the magic happens */
+  argp_parse(&argp, argc, argv, 0, 0, &arguments);
+  if (arguments.cmd != NULL) {
+    void (*cmd)(void) = arguments.cmd;
+    (*cmd)();
+  }
 }
